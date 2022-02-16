c7bb5c393020dd50144de97da4ef3192c2199c3e
ShivaKumar Duddukuri <shivakumarduddukuri81@gmail.com>
2022-02-16 09:19:13 +0530

    Update WebBeansConfig.java

--- src/main/java/com/shiva/cas/sample/project/spring/web/config/WebBeansConfig.java
+++ src/main/java/com/shiva/cas/sample/project/spring/web/config/WebBeansConfig.java
@@ -1,10 +1,4 @@
-/**
- * Copyright (c) 2013 - 2014 WaveMaker, Inc. All Rights Reserved.
- *
- * This software is the confidential and proprietary information of WaveMaker, Inc.
- * You shall not disclose such Confidential Information and shall use it only in accordance
- * with the terms of the source code license agreement you entered into with WaveMaker, Inc.
- */
+
 package com.shiva.cas.sample.project.spring.web.config;
 
 import java.util.LinkedList;
